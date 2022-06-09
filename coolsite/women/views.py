4d800e258d45b979f126e337ff6ff504bab28002
Farid Shamsov <farid979797@gmail.com>
2022-06-09 18:29:37 +0300

    add registration form

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import AddPostForm
+from .forms import AddPostForm, RegisterUserForm
 from .models import *
 from .utils import *
 
@@ -124,4 +125,16 @@ class WomenCategory(DataMixin, ListView):
 #     }
 #
 #     return render(request, 'women/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm            #in forms.py
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        context.update(c_def)
+        return context
 
