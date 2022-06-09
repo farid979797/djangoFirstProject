0aa2660d24fe3bff1fa6942f22f75fd02eab9611
Farid Shamsov <farid979797@gmail.com>
2022-06-09 19:04:23 +0300

    add authentication form and authentication template

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import AddPostForm, RegisterUserForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm
 from .models import *
 from .utils import *
 
@@ -66,8 +68,8 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):                       
 def contact(request):
     return HttpResponse("Обратная связь")
 
-def login(request):
-    return HttpResponse("Авторизация")
+# def login(request):
+#     return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
@@ -138,3 +140,24 @@ class RegisterUser(DataMixin, CreateView):
         context.update(c_def)
         return context
 
+    def form_valid(self, form):                 #auto login after registration
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        context.update(c_def)
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
